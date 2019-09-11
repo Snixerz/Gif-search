68aa823a2c39b7281d8f5a39aac7a6f690c26315
Snixerz <cakeisew@gmail.com>
2019-09-11 16:32:32 -0700

    Added API key + limit

--- gif-search.py
+++ gif-search.py
@@ -4,15 +4,16 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
 
     # TODO: Make 'params' dict with query term and API key
-
+    params = {"api_key": "LIVDSRZULELA", "query": request.args.get("search_term")}
     # TODO: Make an API call to Tenor using the 'requests' library
-
+    request_gifs = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (params["api_key"], params["query"], 10))
     # TODO: Get the first 10 results from the search results
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
